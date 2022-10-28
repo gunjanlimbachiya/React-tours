5c5940e150d03223716070b0f37f3aedf8409a0e
Gunjan <gunjan.limbachiya@gmail.com>
2022-10-28 15:39:04 +0530

    Updates

---
+++ precache-manifest.5ce8d97fbf35b0759d9e0d757e3f5336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7898e781e810a0a16c8818f929466c7",
+    "url": "/React-tours/index.html"
+  },
+  {
+    "revision": "8ea58369b12cfd17bb4c",
+    "url": "/React-tours/static/css/main.2bd077f7.chunk.css"
+  },
+  {
+    "revision": "37ad8af141a11568c704",
+    "url": "/React-tours/static/js/2.81f486fd.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/React-tours/static/js/2.81f486fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea58369b12cfd17bb4c",
+    "url": "/React-tours/static/js/main.805afe91.chunk.js"
+  },
+  {
+    "revision": "d63ca2d018836987ac50",
+    "url": "/React-tours/static/js/runtime-main.24838542.js"
+  }
+]);
